18f82d351728cbbe770ab1e29d069c0d1886e16d
Matt Dickenson <mcdickenson@gmail.com>
2013-05-26 13:10:07 -0500

    added starter file

---
+++ start.R
@@ -0,0 +1,17 @@
+# load libraries
+require(countrycode)
+require(doBy)
+require(DMwR)
+require(e1071)
+require(lubridate)
+require(separationplot)
+require(verification)
+require(ROCR)
+require(RWeka)
+require(Hmisc)
+
+# define constants
+pathCode = '~/github/gdelt-to-mids' 
+pathData = '~/github/gdelt-to-mids/data' 
+
+
