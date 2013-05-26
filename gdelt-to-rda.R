72edb8d18137d8c23ff5195778c08be7a0a41fdd
Matt Dickenson <mcdickenson@gmail.com>
2013-05-26 13:34:33 -0500

    added script to convert gdelt historical files to rda binary format

---
+++ gdelt-to-rda.R
@@ -0,0 +1,32 @@
+# turn gdelt historical csvs into rda files 
+
+rm(list=ls())
+setwd('~/github/gdelt-to-mids')
+source('start.R')
+
+setwd(pathData)
+
+headers = read.table('CSV.header.historical.txt', sep='\t', as.is=TRUE)
+
+YEARS = c(1979)
+
+for(year in YEARS){
+	print(year)
+
+	varname = paste('gd', year, sep="")
+	datafile = paste(year, '.csv', sep="")
+	readcommand = paste(varname, " = read.csv('", datafile, "', sep='\t', header=F, flush=T, as.is=T)",sep="")
+	eval(parse(text=readcommand))
+
+	checkcommand = paste("print(dim(", varname, "))", sep="")
+	eval(parse(text=checkcommand))
+
+	namecommand = paste("colnames(", varname, ") = headers", sep="")
+	eval(parse(text=namecommand))
+
+	savecommand = paste("save(", varname, ", file='", varname, ".rda')", sep="")
+	eval(parse(text=savecommand))
+
+	rmcommand = paste("rm(", varname, ")", sep="")
+	eval(parse(text=rmcommand))
+}
