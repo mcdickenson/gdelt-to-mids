0d43d7d8c96e6623ac96648fa0579a9934a2df73
Matt Dickenson <mcdickenson@gmail.com>
2013-05-28 18:16:48 -0500

    handled missingness in country code conversion

--- gdelt-mids-merge.R
+++ gdelt-mids-merge.R
@@ -2,24 +2,56 @@ setwd('~/github/gdelt-to-mids')
 source('start.R')
 setwd(pathData)
 
+# gdelt data
+load('gd1992.rda')
+dim(gd1992)
+names(gd1992)
+head(gd1992[,1:25])
+head(gd1992[,36:56])
+
+countries = sort(unique(c(gd1992$Actor1CountryCode, gd1992$Actor2CountryCode)))
+countries
+
+# mids dyadic data
 load('dailydyad.rda')
-dailydyad$Actor1Geo_CountryCode = countrycode(dailydyad$CCodeA, "cown", "iso2c")
-dailydyad$Actor2Geo_CountryCode = countrycode(dailydyad$CCodeB, "cown", "iso2c")
+dim(dailydyad)
+
+dailydyad$Actor1CountryCode = countrycode(dailydyad$CCodeA, "cown", "iso3c")
+dailydyad$Actor2CountryCode = countrycode(dailydyad$CCodeB, "cown", "iso3c")
 head(dailydyad)
-# TODO: handle missingness
-# yugoslavia
+
+# handle missingness
+sort(unique(dailydyad$CCodeA[which(is.na(dailydyad$Actor1CountryCode))]))
+unique(dailydyad$CCodeA[which(!(dailydyad$Actor1CountryCode %in% countries))])
+unique(dailydyad$CCodeB[which(!(dailydyad$Actor2CountryCode %in% countries))])
+length(which(!(dailydyad$Actor2CountryCode %in% countries)))
+
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==325, "ITA", dailydyad$Actor1CountryCode) # ITALY
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==344, "HRV", dailydyad$Actor1CountryCode) # FORMER CROATIA
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==345, "SRB", dailydyad$Actor1CountryCode) # FORMER YUGOSLAVIA
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==346, "BIH", dailydyad$Actor1CountryCode) # FORMER YUGOSLAVIA
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==349, "SVN", dailydyad$Actor1CountryCode) # SLOVENIA
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==360, "ROM", dailydyad$Actor1CountryCode) # ROMANIA
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==731, "PRK", dailydyad$Actor1CountryCode) # N. KOREA
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==713, "TWN", dailydyad$Actor1CountryCode) # TAIWAN
+dailydyad$Actor1CountryCode = ifelse(dailydyad$CCodeA==710, "CHN", dailydyad$Actor1CountryCode) # CHINA
+
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==325, "ITA", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==344, "HRV", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==345, "SRB", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==346, "BIH", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==349, "SVN", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==360, "ROM", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==731, "PRK", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==713, "TWN", dailydyad$Actor2CountryCode)
+dailydyad$Actor2CountryCode = ifelse(dailydyad$CCodeB==710, "CHN", dailydyad$Actor2CountryCode)
+
 
 # TODO: consider de-directing the dyads 
 # (ie duplicating all records in dailydyad and reversing the actor codes)
 
 # merge with gdelt by year 
 
-load('gd1992.rda')
-dim(gd1992)
-names(gd1992)
-head(gd1992[,1:25])
-
-head(gd1992[,36:56])
 
 YEARS = c("1992")
 
@@ -32,7 +64,9 @@ for(year in YEARS){
 	readcommand = paste(varname, " = read.csv('", datafile, "', sep='\t', header=F, flush=T, as.is=T)",sep="")
 	eval(parse(text=readcommand))
 
-	# merge
+	# TODO: add R-formatted date variable to gdelt records 
+
+	# TODO: merge
 	
 	# save and clear 
 	savecommand = paste("save(", varname, ", file='", varname, ".rda')", sep="")
