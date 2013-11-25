21cacb18840c7da38ad63032d3b64aafd72cccf8
Matt Dickenson <mcdickenson@gmail.com>
2013-11-24 20:43:24 -0500

    move from quad table to specific event vars

--- build-trees.R
+++ build-trees.R
@@ -20,22 +20,27 @@ load('output1.rda')
 dim(newdata)
 data = newdata[1:100000, ]
 dim(data)
-
+names(data)
 # todo: consider doing a complete-case analysis
 
 # dvs: HostlevMax, hostile1, ..., hostile5
 
 # tree using current variables
-form_current = as.formula(HostlevMax ~ actorMIL + quad1 + quad2 + quad3 + quad4)
+# form_current = as.formula(HostlevMax ~ actorMIL + quad1 + quad2 + quad3 + quad4)
+form_current = as.formula(HostlevMax ~ actorMIL + 
+	event1 + event2 + event3 + event4 + event5 + 
+	event6 + event7 + event8 + event9 + event10 +
+	event11+ event12+ event13+ event14+ event15 +
+	event16+ event17+ event18+ event19+ event20)
 
-# start = Sys.time()
+start = Sys.time()
 tree_current = rpart(form_current, data=data)
-# runtime = Sys.time() - start
-# runtime
+runtime = Sys.time() - start
+runtime
 
+tree_current
 summary(tree_current)
-
-plot(tree_current)
+printcp(tree_current)
 prp(tree_current)
 
 # todo: consider rpart.control(minsplit=x, cp=y, xval=n, maxdepth=k)
