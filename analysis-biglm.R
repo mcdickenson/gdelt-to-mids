7795118427276901aec9a007386a5c8688f8447d
Matt Dickenson <mcdickenson@gmail.com>
2013-11-24 20:12:52 -0500

    remove old files

--- analysis-biglm.R
+++
@@ -1,5 +0,0 @@
-setwd('~/github/gdelt-to-mids')
-source('start.R')
-setwd(pathData)
-
-train = read.big.matrix('train.csv', type='char', header=TRUE)
--- benchmark-loading.R
+++
@@ -1,7 +0,0 @@
-setwd('~/github/gdelt-to-mids')
-source('start.R')
-setwd(pathData)
-system.time(read.csv('1979.csv', sep='\t', header=F, flush=T, as.is=T))
-system.time(read.csv('1979.csv.gz', sep='\t', header=F, flush=T, as.is=T))
-system.time(load('gd1979.rda'))
-
