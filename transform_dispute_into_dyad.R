1cb54033c2cf1f5a8e5efa090270aad436ff31fb
LaDilettante <aql3@duke.edu>
2014-02-18 11:46:50 -0500

    ready for pull request

--- transform_dispute_into_dyad.R
+++
@@ -1,7 +0,0 @@
-rm(list=ls())
-
-toLoad <- c("countrycode", "dplyr", "data.table", "reshape2")
-lapply(toLoad, library, character.only=TRUE)
-
-data <- read.csv("../data/MID/MIDB_4.0.csv")
-names(data)
